6db2aec9ae89ae13bfb0f63dcefcce0db1857ff3
nigglea <diego.espejo@alumnos.uach.cl>
2023-09-06 13:04:28 -0300

    rename files

--- mbe_train.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-set -e
-
-echo "Experiment 2 -----------"
-echo "Preprocessing Data...."
-python 41_preprocess.py 
-echo "Training ..."
-python 42_train.py
-echo "Done ---------------------------------------------------------------"
